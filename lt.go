b6e99524ca618dae7ebc8c2aaa9cd93c4a857e51
Iron Butt UK <webmaster@ironbutt.co.uk>
2024-05-27 21:36:34 +0100

    Create lt.go

---
+++ lt.go
@@ -0,0 +1,23 @@
+import (
+	"fmt"
+
+	gosyslog "log/syslog"
+
+	"github.com/go-kit/log"
+	"github.com/go-kit/log/level"
+	"github.com/go-kit/log/syslog"
+)
+
+func main() {
+	// Normal syslog writer
+	w, err := gosyslog.New(gosyslog.LOG_INFO, "experiment")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	// syslog logger with logfmt formatting
+	logger := syslog.NewSyslogLogger(w, log.NewLogfmtLogger)
+	logger.Log("msg", "info because of default")
+	logger.Log(level.Key(), level.DebugValue(), "msg", "debug because of explicit level")
+}
